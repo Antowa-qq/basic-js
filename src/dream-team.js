17fea918e266df818634db8548af736188a6a5a9
Antowa-qq <anton-vinsss@mail.ru>
2020-02-17 20:30:55 +0300

    refactor: change check for array

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,6 @@
 module.exports = function createDreamTeam(team) {
 
-  if (Array.isArray(team) !== true) { return false; }
+  if (!Array.isArray(team)) { return false; }
   return team
     .filter(item => typeof (item) === "string")
     .map(item => item.split(' ').join('').toUpperCase()) // deleted escape adn to upper case // or  item.replace(/ /g,'')
