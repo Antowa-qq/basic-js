685a6bb101d762df94a313d573c71fef4484b964
Antowa-qq <anton-vinsss@mail.ru>
2020-02-28 04:40:39 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,11 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+
+module.exports = function repeater(str, options) {
+
+    let { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|' } = options;
+
+    return (String(str) 
+    +(addition + additionSeparator).repeat(additionRepeatTimes).slice(0, -Number(additionSeparator.length))
+    +separator).repeat(repeatTimes).slice(0, -Number(separator.length));
+
 };
-  
+
