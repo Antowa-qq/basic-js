92b22ca47b0ec593963bada1dee8dbbace1e932f
Antowa-qq <anton-vinsss@mail.ru>
2020-03-03 07:18:15 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,63 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+    constructor(dircetMachine = true) {
+
+        this.dircetMachine = dircetMachine;
+
+    }
+    addKey(message, key) {
+        key = key.toLowerCase();
+        let result = '';
+        let j = 0;
+        for (let i = 0; i < message.length; i++) {
+            if (/[a-z]/.test(message[i])) {
+                if (j == key.length) {
+                    j = 0;
+                }
+                result += key[j];
+                j += 1;
+            }
+            else {
+                result += message[i];
+            }
+        }
+        return result;
+    }
+    encrypt(message, key) {
+
+        if (!message || !key) {
+            throw new Error();
+        }
+
+        message = message.toLowerCase();
+
+        key = this.addKey(message, key).split('').map((item, index) => {
+            if (!(/[a-z]/.test(item))) {
+                return item;
+            }
+            return String.fromCharCode((item.charCodeAt() - 97 + message[index].charCodeAt() - 97) % 26 + 97);
+        });
+
+        return this.dircetMachine ? key.join('').toUpperCase() : key.reverse().join('').toUpperCase();
+    }
+    decrypt(message, key) {
+
+        if (!message || !key) {
+            throw new Error();
+        }
+
+        message = message.toLowerCase();
+
+        key = this.addKey(message, key).split('').map((item, index) => {
+            if (!(/[a-z]/.test(item))) {
+                return item;
+            }
+            return String.fromCharCode((message[index].charCodeAt() + 26 - item.charCodeAt()) % 26 + 97);
+        })
+
+        return this.dircetMachine ? key.join('').toUpperCase() : key.reverse().join('').toUpperCase();
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
 }
 
 module.exports = VigenereCipheringMachine;
