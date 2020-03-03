a4b4e8b79e458ea1f73868678d0f868768ffd182
Antowa-qq <anton-vinsss@mail.ru>
2020-03-04 00:51:08 +0300

    refactor: change struct algorithms

--- src/transform-array.js
+++ src/transform-array.js
@@ -8,26 +8,21 @@ module.exports = function transform(arr) {
     let result = [];
 
     for (let i = 0; i < arr.length; i++) {
-        if (controls.includes(arr[i])) {
-            switch (arr[i]) {
-                case '--double-next':
-                    if (i != arr.length - 1) { result.push(arr[i + 1]); }
-                    break;
-                case '--double-prev':
-                    if (i != 0) { result.push(arr[i - 1]); }
-                    break;
-                case '--discard-next':
-                    if (i != arr.length - 1) { i += 1; }
-                    break;
-                case '--discard-prev':
-                    if (i != 0) { result.pop(); }
-                    break;
-                default:
-                    result.push(arr[i]);
-            }
-        }
-        else {
-            result.push(arr[i]);
+        switch (arr[i]) {
+            case '--double-next':
+                if (i != arr.length - 1) { result.push(arr[i + 1]); }
+                break;
+            case '--double-prev':
+                if (i != 0) { result.push(arr[i - 1]); }
+                break;
+            case '--discard-next':
+                if (i != arr.length - 1) { i += 1; }
+                break;
+            case '--discard-prev':
+                if (i != 0) { result.pop(); }
+                break;
+            default:
+                result.push(arr[i]);
         }
     }
 
