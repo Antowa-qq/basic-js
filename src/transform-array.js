3432a3f7c217c23bc70ae94c1d2ac1b9d91dbee4
Antowa-qq <anton-vinsss@mail.ru>
2020-03-03 13:50:40 +0300

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,35 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+
+    if (!Array.isArray(arr)) { throw new Error(); }
+
+    const controls = ['--double-next', '--double-prev', '--discard-next', '--discard-prev'];
+
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (controls.includes(arr[i])) {
+            switch (arr[i]) {
+                case '--double-next':
+                    if (i != arr.length - 1) { result.push(arr[i + 1]); }
+                    break;
+                case '--double-prev':
+                    if (i != 0) { result.push(arr[i - 1]); }
+                    break;
+                case '--discard-next':
+                    if (i != arr.length - 1) { i += 1; }
+                    break;
+                case '--discard-prev':
+                    if (i != 0) { result.pop(); }
+                    break;
+                default:
+                    result.push(arr[i]);
+            }
+        }
+        else {
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
 };
